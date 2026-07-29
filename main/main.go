80657852347c22f15ea4531e46bd6c14c1dfb649
augmaux84 <augustoandrei190909@gmail.com>
2026-07-29 20:20:08 -0300

    feat: adding counter code to the project

--- main/main.go
+++ main/main.go
@@ -2,33 +2,64 @@ package main
 
 import "fmt"
 import "net/http"
-// import "log"
+import "strconv"
 
 func main() {
 	
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// initial tests with the GO language
 
-		if r.FormValue("q") != "" {
-            fmt.Fprintf(w, `
-                <body>
-                    <h1>Hello, %s</h1>
-                </body>
-            `, r.FormValue("q"))
-            return
-        }
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	// 	if r.Method == "POST" {
+    //         fmt.Fprintf(w, `
+    //             <body>
+    //                 <h1>Hello, %s</h1>
+    //             </body>
+    //         `, r.FormValue("q"))
+    //         return
+    //     }
 		
-		fmt.Fprintf(w, `
-			<body>
-				<form action="/" method="GET">
-					<label>Enter your name:</label> <br>
-					<input name="q"> <br>
-					<button type="submit">Submit</button>
-				</form>
-			</body>
-		`)
-
-	})	
+	// 	fmt.Fprintf(w, `
+    //         <body>
+    //             <form action="/" method="POST">
+    //                 <label>Counter</label>
+    //                 <input name="counter" value="1">
+    //                 <button type="submit">Add</button>
+    //             </form>
+    //         </body>
+    //         <a href="/">Reset</a>
+    //     `)
+    //     })
     
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+		    count, _ := strconv.Atoi(r.FormValue("counter"))
+            count++
+            fmt.Fprintf(w, `
+            <body>
+                <form action="/" method="POST">
+                    <label>Counter</label>
+                    <input name="counter" value="%d" readonly>
+                    <button type="submit">Add</button>
+                </form>
+                <a href="/">Reset</a>
+            </body>
+            `, count)
+            return
+		}
+
+        fmt.Fprintf(w, `
+            <body>
+                <form action="/" method="POST">
+                    <label>Counter</label>
+                    <input name="counter" value="1" readonly>
+                    <button type="submit">Add</button>
+                </form>
+
+                <a href="/">Reset</a>
+            </body> 
+        `)
+	})
+
 	http.ListenAndServe(":3000", nil)
-	// log.Fatal(http.ListenAndServe(":3000", nil))
 }
