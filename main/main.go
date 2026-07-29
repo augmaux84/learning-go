17a07af8a0bd2c027f341b4a75350ab8eaf6121d
augmaux84 <augustoandrei190909@gmail.com>
2026-07-29 11:46:45 -0300

    feat: adding the Air tool to the project and learning about net/http

--- main/main.go
+++ main/main.go
@@ -1,7 +1,35 @@
 package main
 
 import "fmt"
+import "net/http"
+// import "log"
 
 func main() {
-	fmt.Println("Hello")
+	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		// if 
+		if r.FormValue("q") != "" {
+            fmt.Fprintf(w, `
+                <body>
+                    <h1>Hello, %s</h1>
+                </body>
+            `, r.FormValue("q"))
+            return
+        }
+		
+		fmt.Fprintf(w, `
+			<body>
+				<form action="/" method="GET">
+					<label>Enter your name:</label> <br>
+					<input name="q"> <br>
+					<button type="submit">Submit</button>
+				</form>
+			</body>
+		`)
+
+	})	
+
+    http.ListenAndServe(":3000", nil)
+	// log.Fatal(http.ListenAndServe(":3000", nil))
 }
